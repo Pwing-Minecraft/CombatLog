dd69f7bbd650ca68332f92988a2aa5fed3648ff5
RednedEpic <redned@retronixgames.x10host.com>
2019-04-01 19:31:53 -0500

    Get plugin to a state where it actually runs

--- src/main/java/me/iiSnipez/CombatLog/ActionBar.java
+++ src/main/java/me/iiSnipez/CombatLog/ActionBar.java
@@ -23,6 +23,8 @@ package me.iiSnipez.CombatLog;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
@@ -107,7 +109,8 @@ public class ActionBar {
             Method m2 = pc.getClass().getDeclaredMethod("sendPacket", c3);
             m2.invoke(pc, ppoc);
 		}catch (Exception ex) {
-			ex.printStackTrace();
+			// Use the Spigot API now
+			player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(msg));
 		}
 	}
 }
